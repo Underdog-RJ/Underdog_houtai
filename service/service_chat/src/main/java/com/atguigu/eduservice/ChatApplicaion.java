f1abfd688f1e2c26487988f9df6ee9c2d6f372f2
RJ <1768976999@qq.com>
2021-05-20 09:42:06 +0800

    添加好友

---
+++ service/service_chat/src/main/java/com/atguigu/eduservice/ChatApplicaion.java
@@ -0,0 +1,15 @@
+package com.atguigu.eduservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"com.atguigu"}) //指定扫描位置
+@MapperScan("com.atguigu.eduservice.mapper")
+public class ChatApplicaion {
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplicaion.class);
+    }
+}
---
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/vo/UcentmentberVo.java
@@ -0,0 +1,59 @@
+package com.atguigu.educenter.vo;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class UcentmentberVo {
+
+    private String id;
+
+
+    private String reqId;
+
+    @ApiModelProperty(value = "微信openid")
+    private String openid;
+
+    @ApiModelProperty(value = "手机号")
+    private String mobile;
+
+    @ApiModelProperty(value = "密码")
+    private String password;
+
+    @ApiModelProperty(value = "昵称")
+    private String nickname;
+
+    @ApiModelProperty(value = "性别 2 女，1 男")
+    private Integer sex;
+
+    @ApiModelProperty(value = "年龄")
+    private Integer age;
+
+    @ApiModelProperty(value = "用户头像")
+    private String avatar;
+
+    @ApiModelProperty(value = "用户签名")
+    private String sign;
+
+    @ApiModelProperty(value = "是否禁用 1（true）已禁用，  0（false）未禁用")
+    private Boolean isDisabled;
+
+    @ApiModelProperty(value = "逻辑删除 1（true）已删除， 0（false）未删除")
+    private Boolean isDeleted;
+
+
+    private Date gmtCreate;
+
+
+    private Date gmtModified;
+
+    private String address;
+
+    private String signature;
+}
