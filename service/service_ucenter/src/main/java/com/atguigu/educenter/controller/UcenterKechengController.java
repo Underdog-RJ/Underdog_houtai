592260291c09d741212871b6d565e6a40a6e75ea
tsy9907 <tsy9907@163.com>
2021-05-27 15:31:25 +0800

    我更新了课程收藏

--- service/service_ucenter/src/main/java/com/atguigu/educenter/controller/UcenterKechengController.java
+++ service/service_ucenter/src/main/java/com/atguigu/educenter/controller/UcenterKechengController.java
@@ -9,6 +9,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 /**
  * <p>
  *  前端控制器
@@ -37,26 +39,24 @@ public class UcenterKechengController{
     }
     //设置是否收藏
     @GetMapping("set_collect")
-    public R setCollect(@RequestParam("userId") String userId,@RequestParam("courseId") String courseId){
-        int flag=0;
+    public R setCollect(@RequestParam("userId") String userId,@RequestParam("courseId") String courseId,@RequestParam("flag") Integer flag){
         QueryWrapper<UcenterKecheng> kechengQueryWrapper = new QueryWrapper<>();
         kechengQueryWrapper.eq("user_id",userId)
                 .eq("course_id",courseId);
-        UcenterKecheng one = ucenterKechengService.getOne(kechengQueryWrapper);
-        if (one != null) {
-            flag=one.getIsCollect();
-        }
-        return R.ok().data("flag",flag);
+        UcenterKecheng ucenterKecheng = new UcenterKecheng();
+        ucenterKecheng.setIsCollect(flag);
+        boolean update = ucenterKechengService.update(ucenterKecheng, kechengQueryWrapper);
+        return R.ok().success(update);
     }
-    //根据用户ID和课程ID查询课程是否被用户收藏
+    //根据用户ID获得所有收藏
     @GetMapping("get_collect")
-    public R getCollect(@RequestParam("userId") String userId){
-        int flag=0;
+    public R getCollectById(@RequestParam("userId") String userId){
         QueryWrapper<UcenterKecheng> kechengQueryWrapper = new QueryWrapper<>();
         kechengQueryWrapper.eq("user_id",userId);
-        ucenterKechengService.list(kechengQueryWrapper);
+        List<UcenterKecheng> list = ucenterKechengService.list(kechengQueryWrapper);
 
-        return R.ok().data("flag",flag);
+        return R.ok().data("list",list);
     }
+
 }
 
